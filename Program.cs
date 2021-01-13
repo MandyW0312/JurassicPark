5234b521ea1eb3970010b7a702eefef1303b87c8
Mandy Wade <lightninggirl413@hotmail.com>
2021-01-13 14:57:44 -0500

    Added Remove and Transfer

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JurassicPark
 {
+    class Dinosaur
+    {
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public DateTime WhenAcquired;
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
+
+    }
     class Program
     {
 
@@ -15,6 +25,8 @@ namespace JurassicPark
         }
         static void Main(string[] args)
         {
+            var dinosaurs = new List<Dinosaur>();
+
             // Welcome the user to the app
             BannerMessage("Welcome to Jurassic Park");
 
@@ -26,6 +38,7 @@ namespace JurassicPark
             {
                 // --- Show the Menu
                 // Display to them the menu Options:
+                Console.WriteLine();
                 Console.WriteLine("Menu: ");
                 Console.WriteLine("VIEW: To View a list of our dinosaurs ");
                 Console.WriteLine("ADD: To Add a new dinosaur to our list ");
@@ -36,6 +49,70 @@ namespace JurassicPark
                 Console.WriteLine();
                 Console.Write("What would you like to do? ");
                 var choice = Console.ReadLine().ToUpper().Trim();
+
+                // If View:
+                if (choice == "VIEW")
+                {
+                    // Show all the dinosaurs in the list, ordered by WhenAcquired.
+                    foreach (var dinosaur in dinosaurs)
+                    { Console.WriteLine($"We acquired this dinosaur on {dinosaur.WhenAcquired}, their name is {dinosaur.Name} they are a {dinosaur.DietType} they weigh {dinosaur.Weight} pounds and they are in Enclosure Number {dinosaur.EnclosureNumber}"); }
+                    // If no dinosaurs are in the park, "Print out" "There aren't any"
+                    if (dinosaurs.Count == 0)
+                    {
+                        Console.WriteLine("We do not have any dinosaurs in our park yet");
+                    }
+                }
+                // If Add: Add a new Dinosaur to the list
+                if (choice == "ADD")
+                {
+                    // Prompt for the Name, DietType, Weight, and EnclosureNumber. WhenAcquired must be set by the code
+                    Console.Write("What is the Name of the New Dinosaur? ");
+                    var newDinosaurName = Console.ReadLine();
+                    Console.Write("What is the Diet Type of the New Dinosaur? ");
+                    var newDinosaurDietType = Console.ReadLine();
+                    Console.Write("What is the Weight of the New Dinosaur? ");
+                    var newDinosaurWeight = int.Parse(Console.ReadLine());
+                    Console.Write("What is the Enclosure Number of the New Dinosaur? ");
+                    var newDinosaurEnclosureNumber = int.Parse(Console.ReadLine());
+                    // Make a New Dinosaur
+                    var newDinosaur = new Dinosaur()
+                    {
+                        Name = newDinosaurName,
+                        DietType = newDinosaurDietType,
+                        Weight = newDinosaurWeight,
+                        EnclosureNumber = newDinosaurEnclosureNumber,
+                    };
+                    // Add the dinosaur to the list
+                    dinosaurs.Add(newDinosaur);
+                }
+                // If Remove: 
+                if (choice == "REMOVE")
+                {
+                    // Prompt the user for the dinosaur's name
+                    Console.Write("What is the Name of the Dinosaur you want to remove? ");
+                    var nameOfDinosaur = Console.ReadLine();
+                    // Find the dinosaur with that name
+                    var foundDinosaur = dinosaurs.Find(dinosaur => dinosaur.Name == nameOfDinosaur);
+                    // Remove the found dinosaur
+                    dinosaurs.Remove(foundDinosaur);
+                }
+                // If Transfer: 
+                if (choice == "TRANSFER")
+                {
+                    // Prompt the user for the dinosaur's name
+                    Console.Write("What is the Name of the Dinosaur you want to Transfer? ");
+                    var nameOfDinosaur = Console.ReadLine();
+                    // Find the dinosaur with that name
+                    var foundDinosaur = dinosaurs.Find(dinosaur => dinosaur.Name == nameOfDinosaur);
+                    // Prompt for the New EnclosureNumber
+                    Console.Write("What Enclosure Number would you like to Transfer the Dinosaur to? ");
+                    // Update the EnclosureNumber of that dinosaur
+                    var newEnclosureNumber = int.Parse(Console.ReadLine());
+                    foundDinosaur.EnclosureNumber = newEnclosureNumber;
+                }
+                // If Summary: Display the number of Carnivores and number of Herbivores  
+
+                // If Quit: Stop the Program
                 // --- If the user's choice is QUIT
                 if (choice == "QUIT")
                 {
@@ -44,21 +121,6 @@ namespace JurassicPark
                 }
                 // ----- Back to loop
             }
-
-            // View: Show all the dinosaurs in the list, ordered by WhenAcquired.
-            // If no dinosaurs are in the park, "Print out" "There aren't any"
-            // Add: Add a new Dinosaur to the list
-            // Prompt for the Name, DietType, Weight, and EnclosureNumber. WhenAcquired must be set by the code
-            // Add the dinosaur to the list
-            // Remove: Prompt the user for the dinosaur's name
-            // Find the dinosaur with that name
-            // Remove the found dinosaur
-            // Transfer: Prompt the user for the dinosaur's name
-            // Find the dinosaur with that name
-            // Prompt for the New EnclosureNumber
-            // Update the EnclosureNumber of that dinosaur
-            // Summary: Display the number of Carnivores and number of Herbivores  
-            // Quit: Stop the Program
 
             // Say Goodbye
             BannerMessage("Thank you for visiting, Please come again.");
