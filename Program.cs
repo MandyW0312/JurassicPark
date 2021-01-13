c8bb220f4a71b50053fe853eef0b51663c082f72
Mandy Wade <lightninggirl413@hotmail.com>
2021-01-13 13:58:38 -0500

    Added menu options

--- Program.cs
+++ Program.cs
@@ -18,10 +18,15 @@ namespace JurassicPark
             // Welcome the user to the app
             BannerMessage("Welcome to Jurassic Park");
 
-
-
             // While the user hasn't chosen to Quit:
             // Display to them the menu Options:
+            Console.WriteLine("What would you like to do? ");
+            Console.WriteLine("VIEW: To View a list of our dinosaurs ");
+            Console.WriteLine("ADD: To Add a new dinosaur to our list ");
+            Console.WriteLine("REMOVE: To Delete a dinosaur from our list ");
+            Console.WriteLine("TRANSFER: To Update the Enclosure Info for one of our current dinosaurs ");
+            Console.WriteLine("SUMMARY: To see how many Carnivores and Herbivores we have ");
+            Console.WriteLine("QUIT: To close the application ");
             // View: Show all the dinosaurs in the list, ordered by WhenAcquired.
             // If no dinosaurs are in the park, "Print out" "There aren't any"
             // Add: Add a new Dinosaur to the list
